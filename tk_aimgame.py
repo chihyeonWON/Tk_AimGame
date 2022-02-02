48925a7a0ce2f3ad844d0b8f8a471ce26cb87dde
chihyeonWON <heungeob1003@gamil.com>
2022-02-02 16:16:01 +0900

    버튼이 클릭됬을 때 실행될 함수 생성과 실행

--- tk_aimgame.py
+++ tk_aimgame.py
@@ -14,8 +14,20 @@ ent = Entry(win)
 ent.grid(column=1, row=0, padx=20, pady=20)
 
 # Button
+
+
+def btn_f():
+    global num_t  # num_t 변수를 전역변수로 지정
+    num_t = int(ent.get())  # 입력창에 입력된 문자열을 숫자로 바꿔서 저장
+    # 3개의 위젯이 요소로 있는 리스트 [label, entry, button]
+    for widget in win.grid_slaves():
+        widget.destroy()  # 위젯 요소를 차례로 파괴합니다.
+    win.geometry("500x500")  # 창의 크기를 500x500로 설정합니다.
+
+
 btn = Button(win)
 btn.config(text="시작")
+btn.config(command=btn_f)
 btn.grid(column=0, row=1, columnspan=2)
 
 win.mainloop()
