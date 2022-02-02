d0cc4ad9eb167713c596e0c73d21a84821055aa1
chihyeonWON <heungeob1003@gamil.com>
2022-02-02 15:53:14 +0900

    표적개수  라벨 생성

--- tk_aimgame.py
+++ tk_aimgame.py
@@ -4,5 +4,9 @@ win.title("AIM_GAME")
 win.geometry("550x150")
 win.option_add("*Font", "궁서 20")
 
+# Label
+lab = Label(win)
+lab.config(text="표적 개수")
+lab.grid(column=0, row=0, padx=20, pady=20)
 
 win.mainloop()
