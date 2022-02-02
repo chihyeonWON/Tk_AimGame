c7802d58983c33916177baeaa08c0e32d52bcff5
chihyeonWON <heungeob1003@gamil.com>
2022-02-02 16:02:19 +0900

    시작 버튼 생성

--- tk_aimgame.py
+++ tk_aimgame.py
@@ -9,4 +9,13 @@ lab = Label(win)
 lab.config(text="표적 개수")
 lab.grid(column=0, row=0, padx=20, pady=20)
 
+# Entry
+ent = Entry(win)
+ent.grid(column=1, row=0, padx=20, pady=20)
+
+# Button
+btn = Button(win)
+btn.config(text="시작")
+btn.grid(column=0, row=1, columnspan=2)
+
 win.mainloop()
