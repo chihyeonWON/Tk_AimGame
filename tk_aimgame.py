7dfd99f3996c9b05d7bcb9c3cd7728a911c8a2da
chihyeonWON <heungeob1003@gamil.com>
2022-02-02 16:19:16 +0900

    랜덤한 버튼을 생성하는 함수를 생성하고 적용

--- tk_aimgame.py
+++ tk_aimgame.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 win = Tk()
 win.title("AIM_GAME")
 win.geometry("550x150")
@@ -15,6 +16,16 @@ ent.grid(column=1, row=0, padx=20, pady=20)
 
 # Button
 
+# 랜덤한 위치에 버튼을 생성하는 함수
+
+
+def ran_btn():
+    btn = Button(win)
+    btn.config(bg="red")
+    btn.place(relx=random.random() rely=random.random())
+
+# 버튼을 클릭했을 때 위젯을 모두 없애고 창크기를 키워주는 함수
+
 
 def btn_f():
     global num_t  # num_t 변수를 전역변수로 지정
@@ -23,6 +34,7 @@ def btn_f():
     for widget in win.grid_slaves():
         widget.destroy()  # 위젯 요소를 차례로 파괴합니다.
     win.geometry("500x500")  # 창의 크기를 500x500로 설정합니다.
+    ran_btn()
 
 
 btn = Button(win)
